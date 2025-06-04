7069f3977571f98b056fa8dda4dbeb0b733d640b
sunpaaaa <sunpaaaa@gmail.com>
2025-06-04 22:59:24 +0900

    my reactv template init

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <h2>React Template 🚀</h2>
+      <p>React + TypeScript + Vite</p>
+      <p>
+        <a href='https://github.com/noSPkeepgoing/React-template'>GitHub</a>
+      </p>
+    </>
+  );
+};
+
+export default Home;
